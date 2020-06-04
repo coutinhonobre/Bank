6803b04383331fd9363e94c332fb4444cc11d55e
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-03 23:07:03 -0300

    Testes AccountTest e Account

--- app/src/main/java/com/github/coutinhonobre/bank/data/model/Account.kt
+++ app/src/main/java/com/github/coutinhonobre/bank/data/model/Account.kt
@@ -1,4 +1,10 @@
 package com.github.coutinhonobre.bank.data.model
 
-data class Account {
+data class Account(
+    val userId: Int,
+    val name: String,
+    val bankAccount: String,
+    val agency: String,
+    val balance: Double
+) {
 }
--- app/src/test/java/com/github/coutinhonobre/bank/AccountTest.kt
+++ app/src/test/java/com/github/coutinhonobre/bank/AccountTest.kt
@@ -1,7 +1,11 @@
 package com.github.coutinhonobre.bank
 
 import com.github.coutinhonobre.bank.data.model.Account
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
+import org.hamcrest.CoreMatchers.*
+
 
 class AccountTest {
 
@@ -9,6 +13,20 @@ class AccountTest {
 
     @Before
     fun inicializar(){
-        account = Account();
+        account = Account(1, "Jose da Silva Teste", "2050", "012314564", 3.3445)
     }
+
+    @Test
+    fun adicionarUmaNovaContaTest(){
+        var accountTest = Account(1, "Jose da Silva Teste", "2050", "012314564", 3.3445)
+        Assert.assertThat(account, `is`(accountTest))
+    }
+
+    @Test
+    fun adicionarUmaNovaContaDiferenteTest(){
+        var accountTest = Account(2, "Igor Coutinho Ferreira Nobre", "3145", "09908809", 1.2345)
+        Assert.assertThat(account, `not`(accountTest))
+    }
+
+
 }
