697e71fec7da8f85f22bb9076e1e87824b03b058
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-03 22:47:20 -0300

    Adicao da Classe User

--- app/src/main/java/com/github/coutinhonobre/bank/data/model/User.kt
+++ app/src/main/java/com/github/coutinhonobre/bank/data/model/User.kt
@@ -1,4 +1,57 @@
 package com.github.coutinhonobre.bank.data.model
 
+import java.util.regex.Pattern
+
 class User {
+
+    lateinit var user: String
+    lateinit var password: String
+    var error: String = ""
+
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+
+
+    fun isValido(): Boolean {
+        if ((user.isNotEmpty() || user.isNotBlank()) && (password.isNotBlank() || password.isNotEmpty())) {
+            return if (validarUsuario()) {
+                validacaoPassword()
+            } else false
+        }
+
+        return false
+
+    }
+
+    private fun validacaoPassword(): Boolean{
+        var valido = false
+        password.contains(Regex("[A-Z]")) &&
+                password.contains(Regex("[0-9]")) &&
+                password.contains(Regex("[^a-zA-Z0-9 ]"))
+
+        if (!valido) this.error = "Senha Invalida"
+
+        return valido
+    }
+
+
+    private fun validarUsuario(): Boolean {
+        var valido = false
+        EMAIL_ADDRESS_PATTERN.matcher(this.user)
+            .matches() || ((this.user.length == 11 || this.user.length == 14) && !this.user.contains(
+            "^[a-Z]"
+        ))
+
+        if (!valido) this.error = "Usuario Invalido"
+
+        return valido
+
+    }
 }
