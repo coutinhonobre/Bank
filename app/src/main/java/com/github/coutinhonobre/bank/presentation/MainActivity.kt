98d3dda3b58244625e798aa2b8312d23d31e04f9
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-04 01:14:34 -0300

    Adicionado layout de login

--- app/src/main/java/com/github/coutinhonobre/bank/presentation/MainActivity.kt
+++ app/src/main/java/com/github/coutinhonobre/bank/presentation/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.github.coutinhonobre.bank
+package com.github.coutinhonobre.bank.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.coutinhonobre.bank.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
