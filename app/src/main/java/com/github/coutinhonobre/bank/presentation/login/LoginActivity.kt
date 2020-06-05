80ce1e2a4e59a950a9b5d24018fe56b5c6feac9b
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-04 22:20:45 -0300

    Adicionado cabeçalho da tela de extrato

--- app/src/main/java/com/github/coutinhonobre/bank/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/github/coutinhonobre/bank/presentation/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.github.coutinhonobre.bank.presentation.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.coutinhonobre.bank.R
+import com.github.coutinhonobre.bank.presentation.extrato.ExtratoActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,6 +13,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        button.setOnClickListener {
+            startActivity(Intent(this, ExtratoActivity::class.java))
+        }
 
     }
 }
