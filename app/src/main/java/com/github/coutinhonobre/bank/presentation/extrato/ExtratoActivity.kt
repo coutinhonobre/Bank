76fd78e525eca2efa13aab1f0a88cc1f4623e440
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-06 22:03:56 -0300

    Adapter e popular lista extrato

--- app/src/main/java/com/github/coutinhonobre/bank/presentation/extrato/ExtratoActivity.kt
+++ app/src/main/java/com/github/coutinhonobre/bank/presentation/extrato/ExtratoActivity.kt
@@ -3,14 +3,20 @@ package com.github.coutinhonobre.bank.presentation.extrato
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.coutinhonobre.bank.R
+import com.github.coutinhonobre.bank.data.model.Statement
 import kotlinx.android.synthetic.main.cabecalho_extrato.*
+import kotlinx.android.synthetic.main.lista_extrato.*
 
 class ExtratoActivity : AppCompatActivity() {
 
     private lateinit var loginViewModel: ExtratoViewModel
+
+    private lateinit var extratoList: MutableList<Statement>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +26,33 @@ class ExtratoActivity : AppCompatActivity() {
 
         loginViewModel.userAccount.observe(this, Observer {
             if (it.isNotEmpty()) {
-                textViewCabecalhoExtratoNome.setText(it[0].name)
-                textViewCabecalhoExtratoContaNumero.setText(it[0].bankAccount)
-                textViewCabecalhoExtratoSaldo.setText(it[0].balance.toString())
+                textViewCabecalhoExtratoNome.text = it[0].name
+                textViewCabecalhoExtratoContaNumero.text = it[0].bankAccount
+                textViewCabecalhoExtratoSaldo.text = it[0].balance.toString()
             }
         })
 
         imageViewCabecalhoExtratoLogout.setOnClickListener {
             onBackPressed()
         }
+
+        extratoList = mutableListOf()
+        for (x in 1..500){
+            extratoList.add(
+                Statement(
+                    title = "title ${x}",
+                    desc = "desc ${x}",
+                    date = "date ${x}",
+                    value = (x * 5).toDouble()
+                )
+            )
+        }
+
+        recyclerViewCabecalhoListaRecentes.layoutManager = LinearLayoutManager(this.applicationContext)
+        recyclerViewCabecalhoListaRecentes.adapter = ExtratoAdapter(extratoList)
+
+
+
     }
 
     override fun onResume() {
---
+++ app/src/main/java/com/github/coutinhonobre/bank/presentation/extrato/ExtratoAdapter.kt
@@ -0,0 +1,34 @@
+package com.github.coutinhonobre.bank.presentation.extrato
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.coutinhonobre.bank.R
+import com.github.coutinhonobre.bank.data.model.Statement
+import kotlinx.android.synthetic.main.card_recentes.view.*
+
+class ExtratoAdapter(var extratoList: MutableList<Statement>): RecyclerView.Adapter<ExtratoAdapter.ExtratoViewHolder>() {
+    class ExtratoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(statement: Statement){
+            statement.let {
+                itemView.apply {
+                    textViewRecentesTitle.text = it.title
+                    textViewRecentesDate.text = it.date
+                    textViewRecentesDesc.text = it.desc
+                    textViewRecentesValue.text = it.value.toString()
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ExtratoViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.card_recentes, parent, false))
+
+    override fun getItemCount() = extratoList.size
+
+    override fun onBindViewHolder(holder: ExtratoViewHolder, position: Int) {
+        holder.bindView(extratoList[position])
+    }
+}
