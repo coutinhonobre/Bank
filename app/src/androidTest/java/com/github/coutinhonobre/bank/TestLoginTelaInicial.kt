ca3c177bf0a5ff7d20d711586f8bae839ddaf541
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-07 23:21:41 -0300

    Efetuar login e Limpar campos de edicao para nao pegar autopreenchimento do banco

--- app/src/androidTest/java/com/github/coutinhonobre/bank/TestLoginTelaInicial.kt
+++ app/src/androidTest/java/com/github/coutinhonobre/bank/TestLoginTelaInicial.kt
@@ -2,14 +2,14 @@ package com.github.coutinhonobre.bank
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.github.coutinhonobre.bank.presentation.login.LoginActivity
+import kotlinx.android.synthetic.main.lista_extrato.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,15 +34,19 @@ class TestLoginTelaInicial {
 
     @Test
     fun checarUserESenhaVazioTest(){
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText(""))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText(""))
+        Espresso.closeSoftKeyboard()
         onView(withId(R.id.button)).perform(click())
         onView(withText("User ou Senha vazios!")).check(matches(isDisplayed()))
     }
 
     @Test
     fun usuarioEmailIncorretoTest(){
-        onView(withId(R.id.textInputEditUser)).perform(typeText("test.com"))
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("test.com"))
         Espresso.closeSoftKeyboard()
-        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("12345678"))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.button)).perform(click())
         onView(withText("Usuario Invalido")).check(matches(isDisplayed()))
@@ -50,9 +54,9 @@ class TestLoginTelaInicial {
 
     @Test
     fun usuarioCpfIncorretoTest(){
-        onView(withId(R.id.textInputEditUser)).perform(typeText("1234"))
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("1234"))
         Espresso.closeSoftKeyboard()
-        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("12345678"))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.button)).perform(click())
         onView(withText("Usuario Invalido")).check(matches(isDisplayed()))
@@ -60,9 +64,9 @@ class TestLoginTelaInicial {
 
     @Test
     fun usuarioSenhaIncorretoTest(){
-        onView(withId(R.id.textInputEditUser)).perform(typeText("test@test.com"))
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("test@test.com"))
         Espresso.closeSoftKeyboard()
-        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("12345678"))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.button)).perform(click())
         onView(withText("Senha Invalida")).check(matches(isDisplayed()))
@@ -70,15 +74,46 @@ class TestLoginTelaInicial {
 
     @Test
     fun usuarioSenhaComCPFIncorretoTest(){
-        onView(withId(R.id.textInputEditUser)).perform(typeText("03496196107"))
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("03496196107"))
         Espresso.closeSoftKeyboard()
-        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("12345678"))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.button)).perform(click())
         onView(withText("Senha Invalida")).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun usuarioSenhaIncorretoComLetraTest(){
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("test@test.com"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("1234A5678"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button)).perform(click())
+        onView(withText("Senha Invalida")).check(matches(isDisplayed()))
+    }
 
+    @Test
+    fun usuarioSenhaIncorretoComSimboloTest(){
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("test@test.com"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("1234&5678"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button)).perform(click())
+        onView(withText("Senha Invalida")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun usuarioSenhaCorretosTest(){
+        onView(withId(R.id.textInputEditUser)).perform(clearText(), typeText("test@test.com"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(clearText(), typeText("1234&567A8"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.progressBar)).check(matches(isDisplayed()))
+        Thread.sleep(5000)
+        onView(withId(R.id.recyclerViewCabecalhoListaRecentes)).check(matches(isDisplayed()))
+
+    }
 
 
 
