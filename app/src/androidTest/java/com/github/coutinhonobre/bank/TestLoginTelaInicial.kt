b3d6f63f4943f6f903bb5d363407db140bda67bf
Igor Nobre <igor.nobre@maximatech.com.br>
2020-06-07 22:59:47 -0300

    Teste usuarios incorretos

--- app/src/androidTest/java/com/github/coutinhonobre/bank/TestLoginTelaInicial.kt
+++ app/src/androidTest/java/com/github/coutinhonobre/bank/TestLoginTelaInicial.kt
@@ -1,9 +1,10 @@
 package com.github.coutinhonobre.bank
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -36,6 +37,28 @@ class TestLoginTelaInicial {
         onView(withId(R.id.button)).perform(click())
         onView(withText("User ou Senha vazios!")).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun usuarioEmailIncorretoTest(){
+        onView(withId(R.id.textInputEditUser)).perform(typeText("test.com"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button)).perform(click())
+        onView(withText("Usuario Invalido")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun usuarioCpfIncorretoTest(){
+        onView(withId(R.id.textInputEditUser)).perform(typeText("1234"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.textInputEditPassword)).perform(typeText("12345678"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.button)).perform(click())
+        onView(withText("Usuario Invalido")).check(matches(isDisplayed()))
+    }
+
+
 
 
 
